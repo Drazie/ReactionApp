568684ec659e20184a3a7410027cfc7249e34d83
Drazie <54319841+Drazie@users.noreply.github.com>
2021-02-17 23:36:01 +0200

    Initial commit

---
+++ Reaction/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Reaction
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        void StartTestButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            
+            var btn = sender as Button;
+            btn.IsVisible = false;
+            Device.StartTimer(TimeSpan.FromSeconds(5), () =>
+            {
+
+                btn.IsVisible = true;
+                return false;
+            });
+        }
+
+        public class ReactionTest
+        {
+            //var tapGestureRecognizer = new TapGestureRecognizer();
+            //tapGestureRecognizer.Tapped += (s, e) => {
+                // handle the tap
+            //};
+        }
+    }
+}
