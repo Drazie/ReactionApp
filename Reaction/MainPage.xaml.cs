8e86457202c281c3991a416cc0e4f317ae40f325
Drazie <54319841+Drazie@users.noreply.github.com>
2021-02-18 13:53:04 +0200

    added: test result, reset button

--- Reaction/MainPage.xaml.cs
+++ Reaction/MainPage.xaml.cs
@@ -18,23 +18,50 @@ namespace Reaction
 
         void StartTestButton_Clicked(System.Object sender, System.EventArgs e)
         {
-            
+            AppLabel.IsVisible = false;
             var btn = sender as Button;
             btn.IsVisible = false;
-            Device.StartTimer(TimeSpan.FromSeconds(5), () =>
-            {
+            var initialTime = DateTime.Now;
+            Random random = new Random();
+            int randomStart = random.Next(1, 5);
+            var timeOfStart = initialTime.AddSeconds(randomStart);
 
-                btn.IsVisible = true;
+
+            Device.StartTimer(TimeSpan.FromSeconds(randomStart), () =>
+            {
+                Main.BackgroundColor = Color.Green;
+
+                var tapGestureRecognizer = new TapGestureRecognizer();
+                tapGestureRecognizer.Tapped += (s, et) => {
+                    Main.BackgroundColor = Color.AliceBlue;
+                    var timeOfClick = DateTime.Now;
+                    var result = timeOfClick - timeOfStart;
+                    resultLabel.Text = $"Your time is {result.Seconds}.{result.Milliseconds}";
+                    resultLabel.IsVisible = true;
+                    AppLabel.IsVisible = true;
+                    sizeGridRow1.IsVisible = true;
+                    NewTestButton.IsVisible = true;
+                    Main.GestureRecognizers.Remove(tapGestureRecognizer);
+                };
+
+                Main.GestureRecognizers.Add(tapGestureRecognizer);
                 return false;
             });
+            
         }
 
-        public class ReactionTest
+        void NewTestButton_Clicked(System.Object sender, System.EventArgs e)
         {
-            //var tapGestureRecognizer = new TapGestureRecognizer();
-            //tapGestureRecognizer.Tapped += (s, e) => {
-                // handle the tap
-            //};
+
+            NewTestButton.IsVisible = false;
+            resultLabel.IsVisible = false;
+            sizeGridRow0.IsVisible = false;
+            sizeGridRow1.IsVisible = false;
+            StartTestButton.IsVisible = true;
+            StartTestButton.IsEnabled = true;
         }
+
+
+
     }
 }
