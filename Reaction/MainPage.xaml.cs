eed55b07c23a55e8fa849072427827682b805881
Drazie <54319841+Drazie@users.noreply.github.com>
2021-02-20 01:05:56 +0200

    Comments to the reaction test and Stats page added

--- Reaction/MainPage.xaml.cs
+++ Reaction/MainPage.xaml.cs
@@ -13,7 +13,10 @@ namespace Reaction
         public MainPage()
         {
             InitializeComponent();
+            
         }
+
+        
 
         void StartTestButton_Clicked(System.Object sender, System.EventArgs e)
         {
@@ -24,29 +27,49 @@ namespace Reaction
             int randomStart = random.Next(1, 5);
             var timeOfStart = initialTime.AddSeconds(randomStart);
 
-
             Device.StartTimer(TimeSpan.FromSeconds(randomStart), () =>
             {
                 Main.BackgroundColor = Color.Green;
 
                 var tapGestureRecognizer = new TapGestureRecognizer();                              
                 tapGestureRecognizer.Tapped += (s, et) => {
-                    Main.BackgroundColor = Color.AliceBlue;
+                    OnTap(Color.AliceBlue);
                     var timeOfClick = DateTime.Now;
                     var result = timeOfClick - timeOfStart;
                     resultLabel.Text = $"Your time is {result.Seconds}.{result.Milliseconds}";
-                    resultLabel.IsVisible = true;
-                    AppLabel.IsVisible = true;
-                    sizeGridRow1.IsVisible = true;
-                    NewTestButton.IsVisible = true;
                     Main.GestureRecognizers.Remove(tapGestureRecognizer);
+                    if (result.Milliseconds > 400 && result.Seconds < 1)
+                    {
+                        resultDescription.Text = "Lol.";
+                    }
+                    else if (result.Milliseconds > 300 && result.Milliseconds < 400 && result.Seconds < 1)
+                    {
+                        resultDescription.Text = "I see the silver in the house.";
+                    }
+                    else if (result.Milliseconds > 180 && result.Milliseconds < 300 && result.Seconds < 1)
+                    {
+                        resultDescription.Text = "Conformist.";
+                    }
+                    else if (result.Milliseconds > 90 && result.Milliseconds < 180 && result.Seconds < 1)
+                    {
+                        resultDescription.Text = "Whait, what?";
+                    }
+                    else if (result.Milliseconds < 90 && result.Seconds < 1)
+                    {
+                        resultDescription.Text = "YOOOLOOOO.";
+                    }
+                    else if (result.Seconds > 1)
+                    {
+                        resultDescription.Text = "Are you sleeping?";
+                    }
                 };
-
                 Main.GestureRecognizers.Add(tapGestureRecognizer);
                 return false;
             });
             
         }
+
+        
 
         void NewTestButton_Clicked(System.Object sender, System.EventArgs e)
         {
@@ -54,8 +77,24 @@ namespace Reaction
             resultLabel.IsVisible = false;
             sizeGridRow0.IsVisible = false;
             sizeGridRow1.IsVisible = false;
+            resultDescription.IsVisible = false;
             StartTestButton.IsVisible = true;
         }
 
+
+        public void OnTap(Color color)
+        {
+            Main.BackgroundColor = color;
+            resultDescription.IsVisible = true;
+            resultLabel.IsVisible = true;
+            AppLabel.IsVisible = true;
+            sizeGridRow1.IsVisible = true;
+            NewTestButton.IsVisible = true;
+        }
+
+        void StatsButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new StatsPage());
+        }
     }
 }
---
+++ Reaction/StatsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Reaction
+{
+    public partial class StatsPage : ContentPage
+    {
+        public StatsPage()
+        {
+            InitializeComponent();
+
+            //var lastResult;
+            //LastResultLabel.Text = $"Your last score is: {lastResult}";
+        }
+    }
+}
